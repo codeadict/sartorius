cdc4c43c444cd58266a47ea026b62e487cb4fddd
andresanies <andres_anies@hotmail.com>
2014-07-31 22:27:51 -0500

    Primer codigo para la iniciar el proyecto

---
+++ Sartorius/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Sartorius
+//
+//  Created by Andres Anies on 7/31/14.
+//  Copyright (c) 2014 gydsystems. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Sartorius/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Sartorius
+//
+//  Created by Andres Anies on 7/31/14.
+//  Copyright (c) 2014 gydsystems. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
